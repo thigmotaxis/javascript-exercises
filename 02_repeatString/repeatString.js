fe3eca6b0b683a7ebef0fb669dbdb6bbf34b4c1a
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-03-23 17:19:23 -0700

    completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let stringRepeated = "";
+  for (let i = 0; i < num; i ++) {
+    stringRepeated += string;
+  }
+  if (num < 0) {return "ERROR"};
+  return stringRepeated;
 };
 
 // Do not edit below this line
