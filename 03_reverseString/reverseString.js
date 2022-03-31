adbf2ec01a5b86fac8641530d7b263194667099a
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-03-31 13:26:02 -0700

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+  let stringReversed = "";
+  let update = userString.length;
+  while (update >= 0) {
+    stringReversed += userString.charAt(update);
+    update -= 1;
+  };
+  return stringReversed;
 };
 
 // Do not edit below this line
