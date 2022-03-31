64e566924ccf3a28feca1564fd9a8a6336568149
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-03-31 14:19:29 -0700

    complete exercixe 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+  let updatedArray = [];
+  for (element of array) {
+    if (elementsToRemove.indexOf(element) === -1) {updatedArray.push(element)
+    }
+  }
+  return updatedArray;
+// create a function that takes an array as its first argument and then some number of additional arguments
+// the function will then check the array for all of the other arguments and and remove those arguments if they are present
+// could run a for loop to check each element against the other arguments, and append to a new array if element doesn't match
 
 };
 
