638cf7b3ad6f07f0751f8e6e7a708e5505198976
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-05 11:18:45 -0700

    complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  return Math.round((fTemp - 32) * 5 / 9 * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  return Math.round((cTemp * 9 / 5 + 32) * 10) / 10
 };
 
 // Do not edit below this line
