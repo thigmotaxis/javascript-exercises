19e3843a4426515380f0309f29cdca949027b1dd
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-22 15:23:35 -0700

    completed exercise #10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(fNumber) {
+  if (fNumber < 1) return "OOPS"
+  let first = 0;
+  let second = 1;
+  for (i = 0; i < fNumber ; i++){
+    if (i%2 === 1) {
+      first += second;
+    };
+    if (i%2 === 0) {
+      second += first;
+    };
+  };
+  return (first > second) ? first : second;
 };
 
 // Do not edit below this line
