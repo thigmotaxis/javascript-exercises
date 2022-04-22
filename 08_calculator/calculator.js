fb85ff40ed9e0eb080f2a03ea97d8c86d6baad07
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-22 13:59:12 -0700

    complete exercise #08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
+// sum should equal 0 when calling calculator.sum([])
+const sum = (inputArray) => inputArray.reduce((total, currentValue) => {
+	total += currentValue;
+	return total;
+}, 0);
 
-const sum = function() {
-	
-};
+const multiply = (inputArray) => inputArray.reduce((product, factor) => {
+	return product *= factor;
+});
 
-const multiply = function() {
+const power = (base, exponent) =>  {return base ** exponent;};
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = (fact) => {
+	let product = 1;
+	for (; fact > 0; fact --) {
+		product *= fact;
+	};
+	return product;
 };
 
 // Do not edit below this line
