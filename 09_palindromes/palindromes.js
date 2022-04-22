4a109ab7f878ff3a221cf9644a3bc3fac0a40858
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-22 14:49:10 -0700

    complete exercise #09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
-};
+const palindromes = (testString) => {
+  const cleanString = testString.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~() ]/gi, "").toLowerCase();
+    for (let fromStart = 0, fromEnd = 1; fromStart < cleanString.length; fromStart ++, fromEnd ++) {
+      if ((cleanString[fromStart] !== cleanString[cleanString.length - fromEnd])) return false;
+  };
+  return true;};
 
 // Do not edit below this line
 module.exports = palindromes;
