2bec802259c498ed57aed162cdae0473c798550c
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-04-22 19:25:36 -0700

    completed exercise #12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,8 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (people) => people.reduce((older, nextPerson) => {
+  if (!older.yearOfDeath) {older.yearOfDeath = new Date().getFullYear();}
+  if (!nextPerson.yearOfDeath) {nextPerson.yearOfDeath = new Date().getFullYear();}
+  return ((older.yearOfDeath - older.yearOfBirth) > (nextPerson.yearOfDeath - nextPerson.yearOfBirth) ? older : nextPerson)
+});
 
 // Do not edit below this line
 module.exports = findTheOldest;
